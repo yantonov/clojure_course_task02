e4076e8e5935b65ea02555e3bbfc8ae8c0d230ac
Yury Antonov <yantonov@yandex.ru>
2013-03-26 23:32:52 +0600

    Worker agents knows only about dispatcher agent, not other worker agents.

--- src/clojure_course_task02/agent_search.clj
+++ src/clojure_course_task02/agent_search.clj
@@ -3,10 +3,14 @@
   (:require [clojure-course-task02.io :as io])
   (import java.io.File))
 
+;;; dispatcher agent send messages to worker agents to process directories
+;;; worker agent knows only about dispatcher agent, not other workers
+
 (def ^:dynamic *workers-count* (.availableProcessors (Runtime/getRuntime)))
 (def workers (atom []))
 (def filtered-files (ref []))
 (def progress (atom 0))
+(def dispatcher-agent (agent nil))
 
 (defn add-filtered-files
   [file-filter files]
@@ -19,16 +23,25 @@
 (defn schedule-worker-jobs [jobs-count]
   (swap! progress + jobs-count))
 
-(defn send-workers [dirs]
+(defn select-worker []
+  (let [worker-index (rem (System/currentTimeMillis) *workers-count*)]
+    (@workers worker-index)))
+
+(defn dispatch-jobs-workers [dirs]
   (schedule-worker-jobs (count dirs))
   (doseq [d dirs]
-    (let [worker-index (rem (System/currentTimeMillis) *workers-count*)
-          worker (@workers worker-index)]
+    (send-off (select-worker) read-dir d)))
 
-      (send-off worker read-dir d))))
-
-(defn worker-job-done [worker]
+(defn job-done [worker]
   (swap! progress dec))
+
+(defn notify-dispatcher-job-done [worker]
+  (send dispatcher-agent
+        (fn [a] (job-done worker))))
+
+(defn notify-dispatcher-schedule-jobs [dirs]
+  (send dispatcher-agent
+        (fn [a] (dispatch-jobs-workers dirs))))
 
 (defn read-dir
   [worker dir]
@@ -39,18 +52,12 @@
                              (io/file-name %))
                        dirs)]
     (add-filtered-files worker files)
-    (send-workers dir-names)
-    (worker-job-done worker)
-    worker)
-  )
+    (notify-dispatcher-schedule-jobs dir-names)
+    (notify-dispatcher-job-done worker)
+    worker))
 
 (defn create-agent [file-filter index]
-  (let [a (agent file-filter)]
-    (add-watch a
-               (keyword (str "watch-" index))
-               (fn [key reference old-state new-state]
-                 nil))
-    a))
+  (agent file-filter))
 
 (defn init-workers [file-filter]
   (let [coll (vec (map (fn [i] (create-agent file-filter i))
@@ -61,8 +68,8 @@
   (reset! progress 0)
   (dosync (ref-set filtered-files []))
   (init-workers file-filter)
-  (send-workers (list dir-to-search))
-  )
+  (notify-dispatcher-schedule-jobs (list dir-to-search))
+  (await dispatcher-agent))
 
 (defn wait-completion []
   (while (not (zero? @progress)) nil))
